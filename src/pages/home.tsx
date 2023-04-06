31dba74a21b8d9a7f33f5f92cbe727b695d2a7e7
Minhaj Abdul Rahim <minhajabdulraheem@gmail.com>
2023-04-06 18:16:15 +0530

    updated footer, and chat textarea

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,9 @@ import { useRouter } from "next/router";
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
-import systemMessages from './api/systemMessages';
-import Header from './Header';
+import systemMessages from "./api/systemMessages";
+import Header from "./Header";
 import Footer from "./Footer";
-
 
 export default function Home() {
   const router = useRouter();
@@ -51,11 +50,11 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Header category={""} />
       <div className={styles.container}>
-        <h1 className="text-xl p-4">Select a Category</h1>
-        <div className="h-full grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-16 pt-4">
+        <h1 className="p-4 text-xl">Select a Category</h1>
+        <div className="grid h-full grid-cols-2 gap-8 pt-4 md:grid-cols-3 md:gap-16">
           {systemMessages.map((item, index) => (
             <div
-              className="cursor-pointer rounded-xl overflow-hidden hover:transform hover:scale-110 rounded-md transition relative aspect-w-1 aspect-h-1"
+              className="relative overflow-hidden transition cursor-pointer rounded-xl hover:transform hover:scale-110 aspect-w-1 aspect-h-1"
               key={index}
               onClick={() => handleClick(item)}
             >
@@ -64,13 +63,15 @@ export default function Home() {
                 className="absolute bottom-0 z-10 p-4 w-full text-white bg-gradient-to-t from-[#333333]"
               >
                 <h3 className="text-lg">{item.category}</h3>
-                <p className="text-xs font-light text-opacity-60">{item.description}</p>
+                <p className="text-xs font-light text-opacity-60">
+                  {item.description}
+                </p>
               </div>
               <img
                 id="photo"
                 src={item.image}
                 alt={item.category}
-                className="relative z-0 w-full h-full object-cover opacity-60"
+                className="relative z-0 object-cover w-full h-full opacity-60"
               />
             </div>
           ))}
